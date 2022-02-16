85e01ec0da0fbfe57b58a44195a80510edbbc43c
Mario Conti <marioconti91@gmail.com>
2022-02-16 17:07:05 -0300

    se corrige bug

--- src/container/container.jsx
+++ src/container/container.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-scroll";
 import { Header } from "../header/header";
 import { Main } from "../main/main";
 import { Footer } from "../footer/footer";
